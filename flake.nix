47052ad72f0ff278750eee7cd24bbe0fea20d4c4
Enzo Amorim <enzo.amorim@numih.com>
2026-02-18 11:41:00 -0300

    add nix flake for reproducible builds

---
+++ flake.nix
@@ -0,0 +1,57 @@
+{
+  description = "Chess game built with SFML";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        packages.default = pkgs.stdenv.mkDerivation {
+          pname = "chess-app";
+          version = "0.1.0";
+
+          src = ./.;
+
+          nativeBuildInputs = with pkgs; [
+            cmake
+            makeWrapper
+          ];
+
+          buildInputs = with pkgs; [
+            sfml
+          ];
+
+          configurePhase = ''
+            cmake -S . -B build -DCMAKE_BUILD_TYPE=Release
+          '';
+
+          buildPhase = ''
+            cmake --build build
+          '';
+
+          installPhase = ''
+            mkdir -p $out/bin $out/share/chess-app
+            cp build/ChessApp $out/bin/.ChessApp-unwrapped
+            cp -r etc $out/share/chess-app/
+
+            makeWrapper $out/bin/.ChessApp-unwrapped $out/bin/ChessApp \
+              --chdir $out/share/chess-app
+          '';
+        };
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            cmake
+            sfml
+            gcc
+          ];
+        };
+      }
+    );
+}
